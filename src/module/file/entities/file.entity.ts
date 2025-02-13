221a5292dcc6f8f54f45c7798d29a0bf5a951d80
马军 <majun@xiwicloud.com>
2025-02-13 11:02:28 +0800

    ✨ feat:加file.entity

--- src/module/file/entities/file.entity.ts
+++ src/module/file/entities/file.entity.ts
@@ -1,1 +1,17 @@
-export class Upload {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { BaseEntity } from '@/common/entities/base'
+
+@Entity('file')
+export class FileEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  url: string
+
+  @Column()
+  name: string
+
+  @Column()
+  project_id: string
+}
