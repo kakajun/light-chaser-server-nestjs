1029d3d42507329c558d01c2271d185e34887867
马军 <majun@xiwicloud.com>
2025-02-13 17:20:57 +0800

    fix: 去掉不要的

--- src/module/dbExecutor/dbExecutor.module.ts
+++ src/module/dbExecutor/dbExecutor.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common'
 import { DbExecutorService } from './dbExecutor.service'
-import { TypeOrmModule } from '@nestjs/typeorm'
 import { DbExecutorController } from './dbExecutor.controller'
-// import { DbExecutorEntity } from './entities/dbExecutor.entity'
 
 @Module({
-  // imports: [TypeOrmModule.forFeature([DbExecutorEntity])],
   controllers: [DbExecutorController],
   providers: [DbExecutorService],
 })
